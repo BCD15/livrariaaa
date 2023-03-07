0e4a3b6b94bc239103c8e49e0ad187cbc0b635b4
BCD15 <bruno69correadias@gmail.com>
2023-03-07 14:59:39 -0300

    criação da model editora

--- livraria/models.py
+++ livraria/models.py
@@ -2,3 +2,16 @@ from django.db import models
 
 class Categoria(models.Model):
     descrição = models.CharField(max_length=100)
+
+    ...
+    def __str__(self):
+        return f"{self.descrição} ({self.id})"
+    
+...
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+    site = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
