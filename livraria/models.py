8f5617136d2eb2951ee0378a73f9d5bf8aa3a32c
BCD15 <bruno69correadias@gmail.com>
2023-03-17 14:17:01 -0300

    Criação Autor

--- livraria/models.py
+++ livraria/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Categoria(models.Model):
     descrição = models.CharField(max_length=100)
 
-    ...
     def __str__(self):
         return f"{self.descrição} ({self.id})"
     
@@ -15,3 +14,15 @@ class Editora(models.Model):
 
     def __str__(self):
         return self.nome
+
+...
+
+class Autor (models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+    
+    class Meta:
+        verbose_name_plural = "Autores"
