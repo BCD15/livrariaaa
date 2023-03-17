2bfe9a82b7bc1d732a276e7e94d251725c8de609
BCD15 <bruno69correadias@gmail.com>
2023-03-17 15:06:57 -0300

    erro

--- livraria/models.py
+++ livraria/models.py
@@ -4,9 +4,8 @@ class Categoria(models.Model):
     descrição = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.descrição} ({self.id})"
-    
-...
+        return f"{self.descrição} ({self.id})"   
+
 
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
@@ -15,7 +14,6 @@ class Editora(models.Model):
     def __str__(self):
         return self.nome
 
-...
 
 class Autor (models.Model):
     nome = models.CharField(max_length=255)
@@ -27,15 +25,15 @@ class Autor (models.Model):
     class Meta:
         verbose_name_plural = "Autores"
 
-...
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255) 
     isbn = models.CharField(max_length=32, null=True, blank=True)
     quantidade = models.IntegerField(default=0, null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros")
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
 
     def __str__(self):
         return f"{self.titulo} ({self.quantidade})"
     
-... 
