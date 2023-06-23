5ccfea230cc6576ba11938be11c28f4344ba127c
BCD15 <bruno69correadias@gmail.com>
2023-06-23 15:14:39 -0300

    aula 13

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,12 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 from livraria.views import CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
 
 router = DefaultRouter()
@@ -13,6 +19,8 @@ router.register(r"livro", LivroViewSet)
 
 
 urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path('admin/', admin.site.urls),
     path("", include(router.urls)),
 ]
--- livraria/models.py
+++ livraria/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 class Categoria(models.Model):
     descrição = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.descrição} ({self.id})"   
+        return f"{self.descrição} ({self.id})"
 
 
 class Editora(models.Model):
@@ -15,26 +16,31 @@ class Editora(models.Model):
         return self.nome
 
 
-class Autor (models.Model):
+class Autor(models.Model):
     nome = models.CharField(max_length=255)
     email = models.EmailField(null=True, blank=True)
 
     def __str__(self):
         return self.nome
-    
+
     class Meta:
-        verbose_name_plural = "Autores"     
+        verbose_name_plural = "Autores"
 
 
 class Livro(models.Model):
-    titulo = models.CharField(max_length=255) 
+    titulo = models.CharField(max_length=255)
     isbn = models.CharField(max_length=32, null=True, blank=True)
     quantidade = models.IntegerField(default=0, null=True, blank=True)
-    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
-    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros")
-    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
+    preco = models.DecimalField(
+        max_digits=7, decimal_places=2, default=0, null=True, blank=True
+    )
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros"
+    )
+    editora = models.ForeignKey(
+        Editora, on_delete=models.PROTECT, related_name="livros"
+    )
     autores = models.ManyToManyField(Autor, related_name="livros")
 
     def __str__(self):
         return f"{self.titulo} ({self.quantidade})"
-    
