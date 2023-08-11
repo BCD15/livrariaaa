a52df38638a8c9d0a20a383b3abc2951d37456c8
BCD15 <bruno69correadias@gmail.com>
2023-08-11 15:03:39 -0300

    Aulas 25 e 24

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 
-from .models import Autor, Categoria, Editora, Livro
+from .models import Autor, Categoria, Compra, Editora, Livro, ItensCompra
 
 admin.site.register(Autor)
 
 admin.site.register(Categoria)
 
+admin.site.register(Compra)
+
 admin.site.register(Editora)
 
 admin.site.register(Livro)
+
+admin.site.register(ItensCompra)
+
+
+
--- livraria/serializers/livro.py
+++ livraria/serializers/livro.py
@@ -4,7 +4,6 @@ from livraria.models import Livro
 from uploader.models import Image
 from uploader.serializers import ImageSerializer
 
-
 class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
