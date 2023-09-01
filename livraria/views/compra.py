de2ccf328148efc5e7cc3480bc7660758c43209c
BCD15 <bruno69correadias@gmail.com>
2023-09-01 14:47:27 -0300

    aula 27

--- livraria/views/compra.py
+++ livraria/views/compra.py
@@ -3,7 +3,6 @@ from rest_framework.viewsets import ModelViewSet
 from livraria.models import Compra
 from livraria.serializers import CompraSerializer
 
-
 class CompraViewSet(ModelViewSet):
     queryset = Compra.objects.all()
     serializer_class = CompraSerializer
